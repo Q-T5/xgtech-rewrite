3b01cc534239a5072bd6fe3778a27baee9e2733a
Q-T5 <bikathimartin@outlook.com>
2023-05-19 14:39:54 +0300

    Import and use plugin configurations

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import { createPinia } from "pinia";
+import router from "./plugins/router";
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+
+createApp(App).use(vuetify).use(pinia).use(router).mount("#app");
