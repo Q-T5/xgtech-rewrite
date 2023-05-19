777ddaad1f8941fa9a5612eca0c38e58ceb4413d
Q-T5 <bikathimartin@outlook.com>
2023-05-19 23:13:33 +0300

    Add look of home view component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,36 @@
 <template>
-	<h1>Welcome Home</h1>
+	<div class="h-screen flex flex-col pl-5 justify-center">
+		<div>
+			<h1 class="text-8xl font-alice">XGAMER</h1>
+			<h2 class="text-6xl font-alice">Technologies</h2>
+			<p class="mt-5 text-2xl font-nunito">
+				The future of all gaming. Home of all video games.
+			</p>
+		</div>
+		<div class="mt-5">
+			<v-btn
+				size="large"
+				append-icon="mdi-gamepad-circle"
+				variant="outlined"
+				@click="router.push({ name: 'GamesView' })"
+				color="primary">
+				Browse Available Games
+			</v-btn>
+		</div>
+	</div>
 </template>
 
 <script lang="js">
-	export default {
-	    name: "HomeView",
-	    setup() {
+	import { useRouter } from 'vue-router';
 
-	    }
-	}
+		export default {
+			name: "HomeView",
+			setup() {
+				const router = useRouter();
+
+				return {
+					router
+				}
+			}
+		}
 </script>
