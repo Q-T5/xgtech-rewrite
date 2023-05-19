d6bc5ea1903c0bb8e3ad2db0abd9cb4e28626943
Q-T5 <bikathimartin@outlook.com>
2023-05-19 15:27:42 +0300

    Add package for app components

---
+++ src/components/SideNavbarComp.vue
@@ -0,0 +1,14 @@
+<template>
+	<nav class="w-1/5">
+		<h1>side navbar</h1>
+	</nav>
+</template>
+
+<script lang="js">
+	export default {
+	    name: "SideNavbarComp",
+	       setup() {
+
+	       }
+	}
+</script>
