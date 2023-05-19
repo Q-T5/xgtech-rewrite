2a7ec22bd10e5ef9e287022605afb9c67b2a013b
Q-T5 <bikathimartin@outlook.com>
2023-05-19 23:08:08 +0300

    Add app themes

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -10,6 +10,37 @@ const vuetify = createVuetify({
 	icons: {
 		defaultSet: "mdi",
 	},
+	theme: {
+		defaultTheme: "customDark",
+		themes: {
+			customDark: {
+				dark: true,
+				colors: {
+					background: "#282E57",
+					surface: "#242733",
+					primary: "#df8eff",
+					secondary: "#b7f1ff",
+					error: "#F73826",
+					info: "#56A3CD",
+					success: "#5CEB95",
+					warning: "#EA7D10",
+				},
+			},
+			customLight: {
+				dark: false,
+				colors: {
+					background: "#EAE7F3",
+					surface: "#EAE7F3",
+					primary: "#069e70",
+					secondary: "#b2ffdb",
+					error: "#EF3966",
+					info: "#97DFF2",
+					success: "#0F6C48",
+					warning: "#F5AA47",
+				},
+			},
+		},
+	},
 });
 
 export default vuetify;
