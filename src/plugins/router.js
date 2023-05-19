d51627a8debb20adbbb1a9a18a6b8b34e439bcfc
Q-T5 <bikathimartin@outlook.com>
2023-05-19 15:30:18 +0300

    Add base routes

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,8 +1,39 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import GamesView from "../views/GamesView.vue";
+import ProductsView from "../views/ProductsView.vue";
+import ServicesView from "../views/ServicesView.vue";
+import ContactUsView from "../views/ContactUsView.vue";
 
 const router = createRouter({
 	history: createWebHistory(),
-	routes: [],
+	routes: [
+		{
+			path: "/xgtech-rewrite/",
+			component: HomeView,
+			name: "HomeView",
+		},
+		{
+			path: "/xgtech-rewrite/games/",
+			component: GamesView,
+			name: "GamesView",
+		},
+		{
+			path: "/xgtech-rewrite/products/",
+			component: ProductsView,
+			name: "ProductsView",
+		},
+		{
+			path: "/xgtech-rewrite/services/",
+			component: ServicesView,
+			name: "ServicesView",
+		},
+		{
+			path: "/xgtech-rewrite/contact-us/",
+			component: ContactUsView,
+			name: "ContactView",
+		},
+	],
 });
 
 export default router;
