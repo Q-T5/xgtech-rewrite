978145cb5976d2f5ca33294761f2b605f0f3e3a4
Q-T5 <bikathimartin@outlook.com>
2023-05-19 23:14:05 +0300

    Add two new views

---
+++ src/views/AboutUsView.vue
@@ -0,0 +1,12 @@
+<template>
+	<h1>Welcome to about us</h1>
+</template>
+
+<script lang="js">
+	export default {
+	    name: "AboutUsView",
+	    setup() {
+
+	    }
+	}
+</script>
---
+++ src/views/GamingComputersView.vue
@@ -0,0 +1,12 @@
+<template>
+	<h1>Welcome to Gaming Computers</h1>
+</template>
+
+<script lang="js">
+	export default {
+	    name: "GamingComputersView",
+	    setup() {
+
+	    }
+	}
+</script>
