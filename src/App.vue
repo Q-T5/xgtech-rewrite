0ed62f0f580b6e89a178e75ddd264fa401a70d9b
Q-T5 <bikathimartin@outlook.com>
2023-05-19 15:29:39 +0300

    Mount views and side navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
-	<h1 class="underline">Hello World</h1>
+	<SideNavbarComp />
+	<main class="w-4/5">
+		<router-view v-slot="{ Component }">
+			<keep-alive>
+				<component :is="Component" />
+			</keep-alive>
+		</router-view>
+	</main>
 </template>
 
 <script>
+	import { RouterView } from "vue-router";
+	import SideNavbarComp from "./components/SideNavbarComp.vue";
+
 	export default {
 		name: "App",
+		components: {
+			SideNavbarComp,
+			RouterView,
+		},
 		setup() {},
 	};
 </script>
