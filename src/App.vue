0a5ac9f938ca3aa33ce265e4ab1a7e6e6c0b99de
Q-T5 <bikathimartin@outlook.com>
2023-05-19 23:11:36 +0300

    Add persistent absolute buttons

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
 	<SideNavbarComp />
-	<main class="w-4/5">
+	<main class="w-4/5 relative h-full">
 		<router-view v-slot="{ Component }">
 			<keep-alive>
 				<component :is="Component" />
 			</keep-alive>
 		</router-view>
 	</main>
+	<div class="absolute bottom-4 right-4 flex flex-col items-center space-y-2">
+		<v-btn
+			density="default"
+			icon="mdi-lightbulb"
+			size="x-small"
+			@click="toggleTheme"></v-btn>
+		<v-btn
+			density="default"
+			icon="mdi-chat"
+			size="small"></v-btn>
+		<v-btn
+			density="default"
+			icon="mdi-cart"></v-btn>
+	</div>
 </template>
+
+<style>
+	html {
+		overflow: auto !important;
+	}
+</style>
 
 <script>
 	import { RouterView } from "vue-router";
 	import SideNavbarComp from "./components/SideNavbarComp.vue";
+	import { useTheme } from "vuetify";
 
 	export default {
 		name: "App",
@@ -19,6 +40,20 @@
 			SideNavbarComp,
 			RouterView,
 		},
-		setup() {},
+		setup() {
+			const theme = useTheme();
+
+			function toggleTheme() {
+				if (theme.global.name.value === "customDark") {
+					theme.global.name.value = "customLight";
+				} else if (theme.global.name.value === "customLight") {
+					theme.global.name.value = "customDark";
+				}
+			}
+
+			return {
+				toggleTheme,
+			};
+		},
 	};
 </script>
