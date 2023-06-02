9e186d99e7e7291cbe1327fd4d91da6dc1328c9a
Q-T5 <bikathimartin@outlook.com>
2023-06-02 08:26:47 +0300

    Change UI look

--- src/components/SideNavbarComp.vue
+++ src/components/SideNavbarComp.vue
@@ -1,28 +1,25 @@
 <template>
-  <nav class="w-1/5 justify-between flex flex-col h-screen overflow-y-scroll">
-    <div class="flex flex-col items-center mt-20">
+  <nav class="p-2 flex border-b-primary fixed top-0 w-full shadow-md border-b-[0.5px] items-center justify-between">
+    <div class="flex">
       <img
         src="../assets/xgamer-logoB.svg"
         alt="app-logo"
-        class="w-32" />
-      <div class="text-center">
-        <h1 class="text-sm font-alice">XGAMERTechnologies</h1>
-        <p class="text-sm font-nunito">The Future of All Gaming</p>
+        class="w-8" />
+      <div>
+        <h1 class="text-sm font-alice ml-2">XGAMERTechnologies</h1>
+        <p class="text-xs font-nunito ml-2">The Future of All Gaming</p>
       </div>
     </div>
-    <div class="flex flex-col items-stretch">
+    <div class="">
       <router-link
         v-for="(item, index) in navigationItems"
         :key="index"
         :to="{ name: item.linkName }"
-        class="p-2 text-center font-nunito"
+        class="p-2 text-center font-nunito mx-0.5"
         :class="route.name === item.linkName ? 'bg-primary' : ''">
         {{ item.linkText }}
       </router-link>
     </div>
-    <p class="text-center text-xs font-nunito">
-      XGAMERTechnologies &copy; 2023
-    </p>
   </nav>
 </template>
 
@@ -31,22 +28,22 @@ import { ref } from 'vue';
    import { useRoute } from 'vue-router';
 
    export default {
-       name: "SideNavbarComp",
-       setup() {
-           const navigationItems = ref([
-               { linkText: "Home", linkName: "HomeView"  },
-               { linkText: "Gaming Computers", linkName: "GamingComputersView"  },
-               { linkText: "Games", linkName: "GamesView"  },
-               { linkText: "Products", linkName: "ProductsView"  },
-               { linkText: "Services", linkName: "ServicesView"  },
-               { linkText: "About Us", linkName: "AboutUsView"  },
-               { linkText: "Contact Us", linkName: "ContactView"  },
-           ]);
-           const route = useRoute();
+    name: "SideNavbarComp",
+    setup() {
+      const navigationItems = ref([
+        { linkText: "Home", linkName: "HomeView"  },
+        { linkText: "Gaming Computers", linkName: "GamingComputersView"  },
+        { linkText: "Games", linkName: "GamesView"  },
+        { linkText: "Products", linkName: "ProductsView"  },
+        { linkText: "Services", linkName: "ServicesView"  },
+        { linkText: "About Us", linkName: "AboutUsView"  },
+        { linkText: "Contact Us", linkName: "ContactView"  },
+      ]);
+      const route = useRoute();
 
-           return {
-               navigationItems, route
-           }
-       }
+      return {
+        navigationItems, route
+      }
+    }
    }
 </script>
