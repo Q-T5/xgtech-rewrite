c884e3d6858cbb6b685bddae6bd00dd9dd80be41
Q-T5 <bikathimartin@outlook.com>
2023-06-02 08:27:09 +0300

    Add submit button

--- src/components/ContactUsFormComp.vue
+++ src/components/ContactUsFormComp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-2 w-1/2">
+  <div class="p-2 w-1/2 rounded-md shadow-md border-[0.5px]">
     <h1 class="font-ubuntu text-lg">Write To Us</h1>
     <v-form>
       <v-text-field
@@ -14,7 +14,7 @@
         :counter="50"
         label="Mail Subject"
         variant="underlined"
-        clearable="true"
+        :clearable="true"
         prepend-icon="mdi-information-outline"
         required></v-text-field>
       <v-text-field
@@ -31,6 +31,12 @@
         variant="underlined"
         prepend-icon="mdi-message"
         :model-value="contactDetails.message"></v-textarea>
+      <v-btn
+        class="w-full"
+        append-icon="mdi-send"
+        elevation="0"
+        >Send Email</v-btn
+      >
     </v-form>
   </div>
 </template>
@@ -41,15 +47,15 @@ export default {
 	name: "ContactUsFormComp",
 	setup() {
 		const contactDetails = reactive({
-				email: "",
-				subject: "",
-				phoneNumber: "",
-				message: ""
-			});
+      email: "",
+      subject: "",
+      phoneNumber: "",
+      message: ""
+    });
 
-			return {
-				contactDetails
-			}
+    return {
+      contactDetails
+    }
 	}
 }
 </script>
