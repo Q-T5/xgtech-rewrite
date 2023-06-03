a178ea44e17f7a64ec6ceb6b8cde208cc695ad84
Q-T5 <bikathimartin@outlook.com>
2023-06-03 13:35:54 +0300

    Update distribution binary

--- dist/assets/CustomerPortalView-5e49c041.js
+++
@@ -1,1 +0,0 @@
-import{O as t,_ as e,r as a,o as s,c as n,w as r,a as c}from"./index-198bb8a1.js";const l={name:"CustomerPortalView",components:{OptionalSlot:t}},p=c("h1",null,"Customer Portal",-1);function _(i,m,u,d,f,C){const o=a("OptionalSlot");return s(),n(o,null,{default:r(()=>[p]),_:1})}const w=e(l,[["render",_]]);export{w as default};
