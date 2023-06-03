3af5a1b2bf5e29507945595ef639e0a847d6ec6b
Q-T5 <bikathimartin@outlook.com>
2023-06-03 15:59:51 +0300

    Mount login component to app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <TopNavbarComp />
+  <TopNavbarComp @open-login="displayLogin = true" />
   <main class="relative">
     <router-view v-slot="{ Component }">
       <keep-alive>
@@ -21,6 +21,7 @@
       density="default"
       icon="mdi-cart"></v-btn>
   </div>
+  <LoginComp v-if="displayLogin"  @close-login="displayLogin = false" />
 
   <!-- Teleported components -->
   <teleport to="#notification-zone">
@@ -40,6 +41,8 @@ import TopNavbarComp from './components/TopNavbarComp.vue';
 import { useTheme } from 'vuetify';
 import NotificationComp from './components/NotificationComp.vue';
 import useNotificationStore from './stores/notification-store';
+import LoginComp from './components/LoginComp.vue';
+import { ref } from 'vue';
 
 export default {
   name: 'App',
@@ -47,10 +50,12 @@ export default {
     TopNavbarComp,
     RouterView,
     NotificationComp,
+    LoginComp,
   },
   setup() {
     const theme = useTheme();
     const { notificationActive } = useNotificationStore();
+    const displayLogin = ref(false);
 
     function toggleTheme() {
       if (theme.global.name.value === 'customDark') {
@@ -63,6 +68,7 @@ export default {
     return {
       toggleTheme,
       notificationActive,
+      displayLogin
     };
   },
 };
