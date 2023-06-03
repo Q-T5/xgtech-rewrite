4b0db78f4bcd2be7aea848d7078c1f1e4a95f8d1
Q-T5 <bikathimartin@outlook.com>
2023-06-03 07:09:56 +0300

    Components now utilize the OptionalSlot for correction

--- src/views/ContactUsView.vue
+++ src/views/ContactUsView.vue
@@ -1,56 +1,59 @@
 <template>
-  <TopInfoBarComp title="Contact Us & Location" />
-  <div class="flex justify-center space-x-2 m-4">
-    <ContactUsFormComp />
-    <div
-      class="w-1/2 p-2 rounded-md shadow-md flex flex-col justify-center border-[0.5px]">
-      <h1 class="font-ubuntu text-lg">Please Note</h1>
-      <ul>
-        <li
-          v-for="(line, index) in noticeLines"
-          class="font-nunito p-1">
-          <v-icon icon="mdi-circle-small" />{{ line }}
-        </li>
-      </ul>
-      <h1 class="font-ubuntu text-lg">Support</h1>
-      <p class="font-nunito p-1">
-        We would love to hear from you! For any questions, assistance, comments,
-        compliments or complaints call us or write us a message through any of
-        the channels
-        <a
-          href="#sm-links"
-          class="underline underline-offset-2 hover:text-primary"
-          >below</a
-        >
-        and we'll try to get back to you as soon as possible...
-      </p>
+  <OptionalSlot>
+    <TopInfoBarComp title="Contact Us & Location" />
+    <div class="flex justify-center space-x-2 m-4">
+      <ContactUsFormComp />
+      <div
+        class="w-1/2 p-2 rounded-md shadow-md flex flex-col justify-center border-[0.5px]">
+        <h1 class="font-ubuntu text-lg">Please Note</h1>
+        <ul>
+          <li
+            v-for="(line, index) in noticeLines"
+            class="font-nunito p-1">
+            <v-icon icon="mdi-circle-small" />{{ line }}
+          </li>
+        </ul>
+        <h1 class="font-ubuntu text-lg">Support</h1>
+        <p class="font-nunito p-1">
+          We would love to hear from you! For any questions, assistance,
+          comments, compliments or complaints call us or write us a message
+          through any of the channels
+          <a
+            href="#sm-links"
+            class="underline underline-offset-2 hover:text-primary"
+            >below</a
+          >
+          and we'll try to get back to you as soon as possible...
+        </p>
+      </div>
     </div>
-  </div>
-  <div class="rounded-md shadow-md m-4 border-[0.5px]">
-    <h1 class="p-2 font-ubuntu text-xl border-b-2 border-b-primary">
-      Our Location
-    </h1>
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8190479725354!2d36.81958547423298!3d-1.282366235620301!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f10d61dd2512b%3A0xa3713de939fa6a28!2sXGAMER%20technologies!5e0!3m2!1sen!2ske!4v1685524242976!5m2!1sen!2ske"
-      style="border: 4"
-      allowfullscreen=""
-      loading="lazy"
-      referrerpolicy="no-referrer-when-downgrade"
-      class="w-full h-[28rem]"></iframe>
-  </div>
-  <ContactUsFooterComp />
+    <div class="rounded-md shadow-md m-4 border-[0.5px]">
+      <h1 class="p-2 font-ubuntu text-xl border-b-2 border-b-primary">
+        Our Location
+      </h1>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8190479725354!2d36.81958547423298!3d-1.282366235620301!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f10d61dd2512b%3A0xa3713de939fa6a28!2sXGAMER%20technologies!5e0!3m2!1sen!2ske!4v1685524242976!5m2!1sen!2ske"
+        style="border: 4"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+        class="w-full h-[28rem]"></iframe>
+    </div>
+    <ContactUsFooterComp />
+  </OptionalSlot>
 </template>
 
 <script lang="js">
 import TopInfoBarComp from '../components/TopInfoBarComp.vue';
 import ContactUsFormComp from '../components/ContactUsFormComp.vue';
 import ContactUsFooterComp from '../components/ContactUsFooterComp.vue';
+import OptionalSlot from '../utils/OptionalSlot.vue';
 import { ref } from 'vue';
 
 export default {
   name: "ContactUsView",
 	components: {
-		TopInfoBarComp, ContactUsFormComp, ContactUsFooterComp
+		TopInfoBarComp, ContactUsFormComp, ContactUsFooterComp, OptionalSlot
 	},
   setup() {
     const noticeLines = ref([
--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,59 +1,65 @@
 <template>
-  <div class="w-full flex justify-center pt-2">
-    <div class="w-[75%] flex justify-center flex-col items-center">
-      <div class="flex space-x-2 bg-warning p-4 m-2 rounded-md">
-        <v-icon
-          icon="mdi-alert"
-          class="text-2xl self-center" />
-        <p class="font-nunito">
-          We offer services for gaming computers, laptops, gaming
-          consoles(e.g. PS4 and XBOX) and hand-held gaming consoles(e.g. PSP).
-          Please expand any of the sections below for a product you own to see
-          each service offered and its pricing.
-        </p>
+  <OptionalSlot>
+    <div class="w-full flex justify-center pt-2">
+      <div class="w-[75%] flex justify-center flex-col items-center">
+        <div class="flex space-x-2 bg-warning p-4 m-2 rounded-md">
+          <v-icon
+            icon="mdi-alert"
+            class="text-2xl self-center" />
+          <p class="font-nunito">
+            We offer services for gaming computers, laptops, gaming
+            consoles(e.g. PS4 and XBOX) and hand-held gaming consoles(e.g. PSP).
+            Please expand any of the sections below for a product you own to see
+            each service offered and its pricing.
+          </p>
+        </div>
+        <v-expansion-panels
+          variant="popout"
+          class="my-4 w-[75%]">
+          <v-expansion-panel
+            v-for="(service, index) in filteredServicesOffered"
+            :key="index">
+            <v-expansion-panel-title class="font-ubuntu">{{
+              service.serviceName
+            }}</v-expansion-panel-title>
+            <v-expansion-panel-text>
+              <v-table>
+                <thead>
+                  <tr class="font-ubuntu">
+                    <th class="text-left">Number</th>
+                    <th class="text-left">Service</th>
+                    <th class="text-left">Component</th>
+                    <th class="text-left">Cost(Ksh)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr
+                    class="font-nunito"
+                    v-for="(serv, index) in service.options"
+                    :key="index">
+                    <td>{{ (index += 1) }}</td>
+                    <td>{{ serv.action }}</td>
+                    <td>{{ serv.component }}</td>
+                    <td>{{ serv.cost }}</td>
+                  </tr>
+                </tbody>
+              </v-table>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
       </div>
-      <v-expansion-panels
-        variant="popout"
-        class="my-4 w-[75%]">
-        <v-expansion-panel
-          v-for="(service, index) in filteredServicesOffered"
-          :key="index">
-          <v-expansion-panel-title class="font-ubuntu">{{
-            service.serviceName
-          }}</v-expansion-panel-title>
-          <v-expansion-panel-text>
-            <v-table>
-              <thead>
-                <tr class="font-ubuntu">
-                  <th class="text-left">Number</th>
-                  <th class="text-left">Service</th>
-                  <th class="text-left">Component</th>
-                  <th class="text-left">Cost(Ksh)</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr
-                  class="font-nunito"
-                  v-for="(serv, index) in service.options"
-                  :key="index">
-                  <td>{{ (index += 1) }}</td>
-                  <td>{{ serv.action }}</td>
-                  <td>{{ serv.component }}</td>
-                  <td>{{ serv.cost }}</td>
-                </tr>
-              </tbody>
-            </v-table>
-          </v-expansion-panel-text>
-        </v-expansion-panel>
-      </v-expansion-panels>
     </div>
-  </div>
+  </OptionalSlot>
 </template>
 
 <script lang="js">
-import useServicesComposable from "../composables/ServicesViewCompos.js"
+import useServicesComposable from "../composables/ServicesViewCompos.js";
+import OptionalSlot from "../utils/OptionalSlot.vue";
 export default {
     name: "ServicesView",
+    components: {
+      OptionalSlot
+    },
     setup() {
       const { filteredServicesOffered } = useServicesComposable();
 
