2abb258775529c369e1d1a95ec29b053b86ee4a9
Q-T5 <bikathimartin@outlook.com>
2023-06-03 16:03:57 +0300

    Update distibution binary

---
+++ assets/CustomerPortalView-6ee8dc23.js
@@ -0,0 +1,1 @@
+import{U as n,_ as t,r as e,o as r,c as s}from"./index-2d122d80.js";const c={name:"CustomerPortalView",components:{UnderConstruction:n}};function a(p,_,i,m,u,d){const o=e("UnderConstruction");return r(),s(o)}const l=t(c,[["render",a]]);export{l as default};
