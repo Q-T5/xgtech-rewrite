487b84ec7057cb87b670951c60b01add0ab1a277
Q-T5 <bikathimartin@outlook.com>
2023-06-03 07:09:18 +0300

    Create component to handle top margin issue

---
+++ src/utils/OptionalSlot.vue
@@ -0,0 +1,13 @@
+<!-- 
+    This is a correctional component. Its duty is to fix the issue caused by the navbar being fixed to
+    the top of the screen, which in turn causes the rest of the content to start behind the navbar.
+    The 'mt-12' class fixes this issue by providing a margin that ensures the content  starts from
+    below the navbar.
+    This component is optionally called by anyone who wants this margin
+ -->
+
+<template>
+  <div class="h-fit mt-12">
+    <slot></slot>
+  </div>
+</template>
