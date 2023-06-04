76956aac985648d24265df1d73a5524c9cf7eb4d
Q-T5 <bikathimartin@outlook.com>
2023-06-04 13:10:58 +0300

    Add responsive screen size configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{vue,js}"],
-	theme: {
-		extend: {
-			fontFamily: {
-				abezee: ["ABeeZee", "sans-serif"],
-				alice: ["Alice", "serif"],
-				nunito: ["Nunito", "sans-serif"],
-				ubuntu: ["Ubuntu", "sans-serif"],
-			},
-		},
-	},
-	plugins: [],
+  content: ['./index.html', './src/**/*.{vue,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        abezee: ['ABeeZee', 'sans-serif'],
+        alice: ['Alice', 'serif'],
+        nunito: ['Nunito', 'sans-serif'],
+        ubuntu: ['Ubuntu', 'sans-serif'],
+      },
+      // screen sizes according to the Vuetify docs: => https://vuetifyjs.com/en/features/display-and-platform/
+      screens: {
+        smartphones: '600px',
+        tablets: '960px',
+        laptops: '1280px',
+        desktops: '1920px',
+        wideDesktop: '2560px',
+      },
+    },
+  },
+  plugins: [],
 };
