9104fa6188e57a70636983e78327bd24ebbcc35f
Q-T5 <bikathimartin@outlook.com>
2023-06-04 11:34:31 +0300

    Utilize optional slot

--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -1,13 +1,17 @@
 <template>
-  <ProductDisplayComp />
+  <OptionalSlot>
+    <ProductDisplayComp />
+  </OptionalSlot>
 </template>
 
 <script lang="js">
 import ProductDisplayComp from '../components/ProductDisplayComp.vue';
+import OptionalSlot from '../utils/OptionalSlot.vue';
+
 export default {
     name: "GamesView",
 	components: {
-		ProductDisplayComp
+		ProductDisplayComp, OptionalSlot
 	},
     setup() {
 
