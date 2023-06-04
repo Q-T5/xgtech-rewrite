c93addfb9bad0ec5aaf39274646c2c53d098d731
Q-T5 <bikathimartin@outlook.com>
2023-06-04 10:28:32 +0300

    Add background wallpaper for homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="h-screen flex flex-col pl-5 justify-center">
-    <div>
+  <div class="h-screen flex flex-col pl-5 justify-center bg-[url('https://files.fm/thumb_show.php?i=k9558qx35')] object-cover">
+    <div  class="ml-6">
       <h1 class="text-8xl font-alice">XGAMER</h1>
       <h2 class="text-6xl font-alice">Technologies</h2>
       <p class="mt-5 text-2xl font-nunito">
         The future of all gaming. Home of all video games.
       </p>
     </div>
-    <div class="mt-5">
+    <div class="mt-5 ml-6">
       <v-btn
         size="large"
         append-icon="mdi-gamepad-circle"
