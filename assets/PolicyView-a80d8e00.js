199f49083ea688395828c226cc86889d2da305f6
Q-T5 <bikathimartin@outlook.com>
2023-06-05 11:47:49 +0300

    Update distribution binary

---
+++ assets/PolicyView-a80d8e00.js
@@ -0,0 +1,1 @@
+import{U as n,_ as e,r as t,o as c,c as r}from"./index-963d3891.js";const s={name:"PolicyView",components:{UnderConstruction:n}};function a(i,p,_,d,m,u){const o=t("UnderConstruction");return c(),r(o)}const l=e(s,[["render",a]]);export{l as default};
