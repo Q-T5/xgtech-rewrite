bf0085de8c8e25a1ad362e4ad1911eee8ac443d2
Q-T5 <bikathimartin@outlook.com>
2023-06-05 10:09:36 +0300

    Add temporary log messages

--- src/stores/shoppingcart-store.js
+++ src/stores/shoppingcart-store.js
@@ -4,11 +4,15 @@ import { ref, computed } from 'vue';
 const useShoppingCartStore = defineStore('ShoppingCartStore', () => {
   const shoppingCartItems = ref([]);
   const cartTotal = computed(() => {
-    return shoppingCartItems.value.length;
+    shoppingCartItems.value.length;
   });
 
   function addItemToCart(item) {
     shoppingCartItems.value.push(item);
+    console.log(
+      `Shopping cart now: ${JSON.stringify(shoppingCartItems.value)}`,
+    );
+    console.log(`Cart total: ${cartTotal.value}`);
   }
 
   function removeItemFromCart(index) {
